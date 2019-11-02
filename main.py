500b0904761a4024d09974023e5bea05252b216b
Marco David <m.david@jacobs-university.de>
2019-11-02 19:21:34 -0400

    Commit intermediate HNN adaptation in rotating frame.

--- main.py
+++ main.py
@@ -7,59 +7,60 @@ from library.Noise import UhlenbeckNoise
 from library.MagneticField import BField, R_y, theta_linear
 
 
+def theta_periodic(t, args):
+    tf = args['tf']
+    return 4 * np.pi * t / tf
+
+
 def cartesian_product(arrays):
     return np.dstack(np.meshgrid(*arrays, indexing='ij')).reshape(-1, len(arrays))
 
+
 def timeDeriv(t, traj):
-    
     timeDifference = t[1]
     output = []   
     
-        #Do the first and last values manually because they dont follow the equation
-        
-        #First value
-        
+    # Do the first and last values manually because they dont follow the equation
+    # First value
     x = (traj[1][0] - traj[0][0])/timeDifference
     y = (traj[1][1] - traj[0][1])/timeDifference
     z = (traj[1][2] - traj[0][2])/timeDifference
 
     output.append([x, y, z])
         
-    #Loop
+    # Loop
     for i in range(1, len(t) - 1):
-        
         x = (traj[i + 1][0] - traj[i - 1][0])/(2*timeDifference)
         y = (traj[i + 1][1] - traj[i - 1][1])/(2*timeDifference)
         z = (traj[i + 1][2] - traj[i - 1][2])/(2*timeDifference)
-
         output.append([x, y, z])
         
-        
-        #Last Value
-        
+    # Last Value
     x = (traj[len(traj) - 1][0] - traj[len(traj) - 2][0])/timeDifference
     y = (traj[len(traj) - 1][1] - traj[len(traj) - 2][1])/timeDifference
     z = (traj[len(traj) - 1][2] - traj[len(traj) - 2][2])/timeDifference
     output.append([x, y, z])
     
-    return output
+    return np.array(output)
+
 
 class Run(object):
 
-    def __init__(self, var, tau_c, tf):
+    def __init__(self, var, tau_c, tf, T_steps):
         self.mean = 0
         self.var = var
         self.tau_c = tau_c
         self.tf = tf
+        self.T_steps = T_steps
 
         self.noise = UhlenbeckNoise(self.mean, self.var, self.tau_c)
         self.B = BField(R_y, theta_linear, np.array([0, 0, 1]))
-        self.solve = Solve(self.B, self.noise, self.tf, eigenbasis=False)
+        self.solve = Solve(self.B, self.noise, self.tf, eigenbasis=True, steps=self.T_steps)
 
 
 def get_trajectory(params):
-    var, tf, tauc = params
-    run = Run(var, tauc, tf).solve
+    var, tf, tauc, T = params
+    run = Run(var, tauc, tf, T).solve
     return run.times, run.expect
 
 
@@ -72,24 +73,34 @@ def get_trajectory(params):
 
 
 if __name__ == '__main__':
-    # Runs
-    N = 1 # 100
+    # Run ID
+    N = 2
+
+    # Time steps
+    T = 1000
 
     # Sample N values in each dimension
-    var = 0.01 # = 1/100 B
-    tf = 60
+    var = 0 # = 1/100 B
+    tf = 20
     tauc = 5
 
     # MAIN CALL
-    t, traj = get_trajectory((var, tf, tauc))
-    plt.plot(t, traj)
+    t, traj = get_trajectory((var, tf, tauc, T))
+    p = timeDeriv(t, traj)
+    statearray = np.concatenate([t[:, np.newaxis], traj, p], axis=1)
 
-    #plt.legend()
+    # Save position and momentum values to file
+    np.savetxt(f"simulation-results/data{N}.csv", statearray, delimiter=",")
+
+    labels = ['x', 'y', 'z']
+    for i in range(3):
+        plt.plot(t, traj[:, i], label=labels[i])
+
+    plt.legend()
     plt.xlabel("Time $t$")
     plt.ylabel(r"Expectation values $\langle\sigma_\alpha\rangle$")
     plt.title(f"$B = 1$, $\langle\eta^2\\rangle = {var}$, $\\tau_c = {tauc}$")
-    #plt.yscale('log')
-    plt.show()
+    plt.savefig(f'simulation-results/traj-plane{N}.pdf')
 
     #errors = np.array(e_list)
     #means = np.squeeze(errors.mean(axis=0))
--- train.py
+++ train.py
@@ -17,18 +17,17 @@ sys.path.append(PARENT_DIR)
 
 from hnn.nn_models import MLP
 from hnn.hnn import HNN
-from data import get_dataset ## TODO CHANGE
 from hnn.utils import L2_loss, rk4
 
 def get_args():
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--input_dim', default=2, type=int, help='dimensionality of input tensor')
+    parser.add_argument('--input_dim', default=3, type=int, help='dimensionality of input tensor')
     parser.add_argument('--hidden_dim', default=200, type=int, help='hidden dimension of mlp')
     parser.add_argument('--learn_rate', default=1e-3, type=float, help='learning rate')
     parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
     parser.add_argument('--total_steps', default=2000, type=int, help='number of gradient steps')
     parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
-    parser.add_argument('--name', default='pend', type=str, help='only one option right now')
+    parser.add_argument('--name', default='spin', type=str, help='only one option right now')
     parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
     parser.add_argument('--use_rk4', dest='use_rk4', action='store_true', help='integrate derivative with RK4')
     parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
@@ -38,61 +37,60 @@ def get_args():
     parser.set_defaults(feature=True)
     return parser.parse_args()
 
-def train(args):
-  # set random seed
-  torch.manual_seed(args.seed)
-  np.random.seed(args.seed)
+def train(data, args):
+    # set random seed
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
 
-  # init model and optimizer
-  if args.verbose:
-    print("Training baseline model:" if args.baseline else "Training HNN model:")
+    # init model and optimizer
+    if args.verbose:
+        print("Training baseline model:" if args.baseline else "Training HNN model:")
 
-  output_dim = args.input_dim if args.baseline else 2
-  nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity)
-  model = HNN(args.input_dim, differentiable_model=nn_model,
+    output_dim = args.input_dim if args.baseline else 2
+    nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity)
+    model = HNN(args.input_dim, differentiable_model=nn_model,
               field_type=args.field_type, baseline=args.baseline)
-  optim = torch.optim.Adam(model.parameters(), args.learn_rate, weight_decay=1e-4)
+    optim = torch.optim.Adam(model.parameters(), args.learn_rate, weight_decay=1e-4)
 
-  # arrange data
-  data = get_dataset(seed=args.seed)
-  x = torch.tensor( data['x'], requires_grad=True, dtype=torch.float32)
-  test_x = torch.tensor( data['test_x'], requires_grad=True, dtype=torch.float32)
-  dxdt = torch.Tensor(data['dx'])
-  test_dxdt = torch.Tensor(data['test_dx'])
+    # arrange data
+    x = torch.tensor(data['x'], requires_grad=True, dtype=torch.float32)
+    test_x = torch.tensor(data['test_x'], requires_grad=True, dtype=torch.float32)
+    dxdt = torch.Tensor(data['dx'])
+    test_dxdt = torch.Tensor(data['test_dx'])
 
-  # vanilla train loop
-  stats = {'train_loss': [], 'test_loss': []}
-  for step in range(args.total_steps+1):
-    
-    # train step
-    dxdt_hat = model.rk4_time_derivative(x) if args.use_rk4 else model.time_derivative(x)
-    loss = L2_loss(dxdt, dxdt_hat)
-    loss.backward() ; optim.step() ; optim.zero_grad()
-    
-    # run test data
-    test_dxdt_hat = model.rk4_time_derivative(test_x) if args.use_rk4 else model.time_derivative(test_x)
-    test_loss = L2_loss(test_dxdt, test_dxdt_hat)
+    # vanilla train loop
+    stats = {'train_loss': [], 'test_loss': []}
+    for step in range(args.total_steps+1):
 
-    # logging
-    stats['train_loss'].append(loss.item())
-    stats['test_loss'].append(test_loss.item())
-    if args.verbose and step % args.print_every == 0:
-      print("step {}, train_loss {:.4e}, test_loss {:.4e}".format(step, loss.item(), test_loss.item()))
+        # train step
+        dxdt_hat = model.rk4_time_derivative(x) if args.use_rk4 else model.time_derivative(x)
+        loss = L2_loss(dxdt, dxdt_hat)
+        loss.backward() ; optim.step() ; optim.zero_grad()
 
-  train_dxdt_hat = model.time_derivative(x)
-  train_dist = (dxdt - train_dxdt_hat)**2
-  test_dxdt_hat = model.time_derivative(test_x)
-  test_dist = (test_dxdt - test_dxdt_hat)**2
-  print('Final train loss {:.4e} +/- {:.4e}\nFinal test loss {:.4e} +/- {:.4e}'
-    .format(train_dist.mean().item(), train_dist.std().item()/np.sqrt(train_dist.shape[0]),
+        # run test data
+        test_dxdt_hat = model.rk4_time_derivative(test_x) if args.use_rk4 else model.time_derivative(test_x)
+        test_loss = L2_loss(test_dxdt, test_dxdt_hat)
+
+        # logging
+        stats['train_loss'].append(loss.item())
+        stats['test_loss'].append(test_loss.item())
+        if args.verbose and step % args.print_every == 0:
+            print("step {}, train_loss {:.4e}, test_loss {:.4e}".format(step, loss.item(), test_loss.item()))
+
+    train_dxdt_hat = model.time_derivative(x)
+    train_dist = (dxdt - train_dxdt_hat)**2
+    test_dxdt_hat = model.time_derivative(test_x)
+    test_dist = (test_dxdt - test_dxdt_hat)**2
+    print('Final train loss {:.4e} +/- {:.4e}\nFinal test loss {:.4e} +/- {:.4e}'
+        .format(train_dist.mean().item(), train_dist.std().item()/np.sqrt(train_dist.shape[0]),
             test_dist.mean().item(), test_dist.std().item()/np.sqrt(test_dist.shape[0])))
 
-  return model, stats
+    return model, stats
 
 
 def integrate_model(model, t_span, y0, **kwargs):
     def fun(t, np_x):
-        x = torch.tensor(np_x, requires_grad=True, dtype=torch.float32).view(1, 2)
+        x = torch.tensor(np_x, requires_grad=True, dtype=torch.float32).view(1, 3)
         dx = model.time_derivative(x).data.numpy().reshape(-1)
         return dx
 
@@ -100,29 +98,47 @@ def integrate_model(model, t_span, y0, **kwargs):
 
 
 if __name__ == "__main__":
+    # EXPERIMENT
+
+    # Read, format and split data
+    N = 2 # Run ID
+    raw_data = np.loadtxt(f'simulation-results/data{N}.csv', delimiter=',')
+    t, pos, vel = raw_data[:, 0], raw_data[:, 1:4], raw_data[:, 4:7]
+
+    test_split = 0.1
+
+    data = dict()
+    split_ix = int(len(t) * test_split)
+    for arr, k in [(pos, 'x'), (vel, 'dx')]:
+        data[k], data['test_' + k] = arr[:split_ix], arr[split_ix:]
+
+    # TRAIN MODEL
     args = get_args()
-    model, stats = train(args)
+    model, stats = train(data, args)
 
-    t_span = [0, 28]
-    y0 = np.asarray([2.1, 0])
-    kwargs = {'t_eval': np.linspace(t_span[0], t_span[1], 1000), 'rtol': 1e-12}
+    # Integrate Model
+    t_span = [t[0], t[-1]]
+    y0 = np.asarray([0.0, 0.0, 1.0])
+    kwargs = {'t_eval': np.linspace(t_span[0], t_span[1], 1000), 'rtol': 1e-10, 'method': 'RK45'}
+
     hnn_ivp = integrate_model(model, t_span, y0, **kwargs)
-    print("\nSuccess!")
 
-    DPI = 300
-    FORMAT = 'pdf'
-    LINE_SEGMENTS = 10
-    LINE_WIDTH = 2
     import matplotlib.pyplot as plt
 
-    for i, l in enumerate(np.split(hnn_ivp['y'].T, LINE_SEGMENTS)):
-        color = (float(i) / LINE_SEGMENTS, 0, 1 - float(i) / LINE_SEGMENTS)
-        plt.plot(l[:, 0], l[:, 1], color=color, linewidth=LINE_WIDTH)
+    labels = ['x', 'y', 'z']
+    for i in range(3):
+        t = hnn_ivp['t']
+        pos = hnn_ivp['y'][i, :]
+        plt.plot(t, pos, label=labels[i])
 
-    plt.xlabel("$q$", fontsize=14)
-    plt.ylabel("$p$", rotation=0, fontsize=14)
-    plt.title("Hamiltonian NN", pad=10)
+    plt.legend()
+    plt.savefig(f'network-results/model-output{N}.pdf')
     plt.show()
+
+    #plt.xlabel("$q$", fontsize=14)
+    #plt.ylabel("$p$", rotation=0, fontsize=14)
+    #plt.title("Hamiltonian NN", pad=10)
+    #plt.show()
 
     # save
     os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None
