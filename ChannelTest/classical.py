44fcb8c24035fd36531f69b5ab225a48e85f0517
HiveCEO69 <notanemail@github.com>
2019-11-03 10:50:52 -0500

    More stuff

--- ChannelTest/classical.py
+++ ChannelTest/classical.py
@@ -1,6 +1,7 @@
 import numpy as np
 from random import randint
 from statistics import mode
+import os
 # File names
 in_name = 'cat.png'
 out_name = 'cat2.png'
@@ -8,10 +9,10 @@ bw_name = 'blackcat.png'
 
 from PIL import Image 
 
-def convert_to_bw(filename):
+def convert_to_bw(filename,outname):
     image_file = Image.open(filename) # open colour image
     image_file = image_file.convert('1') # convert image to black and white
-    image_file.save('blackcat.png')
+    image_file.save(outname)
 # Read data and convert to a list of bits
 # Convert the list of bits back to bytes and save
 
@@ -32,7 +33,7 @@ def randomize_bw(input_cell, probability):
     else:
         return input_cell
 
-def bw_channel(filename, probability=20, k=2):
+def bw_channel(filename, probability=20, k=2, outname="cats/traing/{k}"):
     im = Image.open(filename) # Can be many different formats.
     pix = im.load()
     dimension = im.size
@@ -42,23 +43,31 @@ def bw_channel(filename, probability=20, k=2):
             pix[i,j] = handle_redundancy(pix[i,j], k, probability,randomize_bw)
 
     # final = Image.fromarray(pix)
-    im.save(f"static/images/classical-{probability}-{2*k+1}.png")
+    im.save(outname)
     # Image.save('result.png')
     # pix[x,y] = value 
 
 if __name__ == "__main__":
-    # convert_to_bw(in_name)
-    prob = 0
-    while prob <= 100:
-        for k in range(6):
+    for k in os.listdir('cats'):
+        # convert_to_bw(f"cats/{k}", f"cats/{k}")
 
-            bw_channel(bw_name, probability=prob, k=k)
-         # bw_channel(bw_name)
-        prob += 10
+        for j in range(1,3):
+            bw_channel(f"cats/{k}", outname=f"cats/train/{j}-{k}", k=0, probability=j)
+
+        
+
+
+
+
+
+    # convert_to_bw(in_name)
+    # prob = 0
+    # while prob <= 100:
+    #     for k in range(6):
+
+    #         bw_channel(bw_name, probability=prob, k=k)
+    #      # bw_channel(bw_name)
+    #     prob += 10
     # data = read_file(in_name)
     # channel(data)
-
-
-
-
 
